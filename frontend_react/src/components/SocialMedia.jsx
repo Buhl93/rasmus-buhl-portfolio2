a2b417ce056d400892a04e37f2feae9417a2439f
Rasmus Jakobsen <buhl.jakobsen@gmail.com>
2022-06-21 19:44:42 +0200

    social media

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,15 +5,24 @@ import { FaFacebookF, FaGithub } from "react-icons/fa";
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsLinkedin />
-      </div>
-      <div>
-        <FaGithub />
-      </div>
-      <div>
-        <BsFacebook />
-      </div>
+      <a
+        href="https://www.linkedin.com/in/rasmus-buhl-jakobsen-686980104/"
+        target="_blank"
+      >
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a href="https://github.com/Buhl93?tab=repositories" target="_blank">
+        <div>
+          <FaGithub />
+        </div>
+      </a>
+      <a href="https://www.facebook.com/rasse10/" target="_blank">
+        <div>
+          <BsFacebook />
+        </div>
+      </a>
     </div>
   );
 };
